480559adcecbc47668165e63e901eb4d81163166
pavelshm10 <pavelsh00@gmail.com>
2025-05-27 16:10:17 +0300

    qwwq

--- clinet/src/App.tsx
+++ clinet/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React11112234333</h1>
+      <h1>Vite + React11112234333www</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
